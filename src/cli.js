b555ff6d9e6efb4e512aa685152f828d7b6b271a
Neri Bez Fontana <nerifontana@gmail.com>
2018-09-12 23:35:30 -0300

    Add foxxy

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,18 @@
 #!/usr/bin/env node
 'use strict';
+const path = require('path');
 const importJsx = require('import-jsx');
+const termImg = require('term-img');
+const terminalImage = require('terminal-image');
 const {h, render} = require('ink');
 
 const ui = importJsx('./ui');
+
+const fallback = async () => {
+	const image = await terminalImage.file(path.join(__dirname, 'fox.png'));
+	console.log(image);
+};
+
+termImg(path.join(__dirname, 'fox.png'), {fallback});
 
 render(h(ui));
